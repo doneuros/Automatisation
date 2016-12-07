d72796c15e61fd24e874952e0be944a7b2ba3a44
Marc <doneuros11@web.de>
2016-12-07 18:29:08 +0100

    Sensitve Storage

---
+++ Service/src/main/java/com/marc/rest/sensitive/SensitiveKeys.java
@@ -0,0 +1,11 @@
+package com.marc.rest.sensitive;
+
+/**
+ * Created by marc on 07.12.16.
+ */
+public class SensitiveKeys {
+
+    public static String owmKey ="";
+    public static String googleApiKey ="";
+
+}
